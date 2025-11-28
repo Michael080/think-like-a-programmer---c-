f00d5c70a21dbc661746671cc2742608c2b940ee
Michael080 <mrollin808@gmail.com>
2025-11-28 14:09:27 -0700

    Create example for demonstrating compilation
    Unit "Variables: Compile & Execute"

---
+++ codecademy_cpp/2.variables/1_lesson/2-compile-and-execute/execute-me.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+
+	std::cout << "Compile me w/ default name a.out via 'g++ execute-me.cpp'\n";
+	// manually spec executable name
+	std::cout << "\nOR: Specify executable file name via 'g++ execute-me.cpp -o executable-name'\n";
+
+}
