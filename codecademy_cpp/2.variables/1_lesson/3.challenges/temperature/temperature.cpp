78200791997b7a9e00dae63bac96895b9b8e2f19
Michael080 <mrollin808@gmail.com>
2025-11-29 12:57:25 -0700

    Complete part 1 of challenge "Temperature"

---
+++ codecademy_cpp/2.variables/1_lesson/3.challenges/temperature/temperature.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/*
+Challenge Description:
+
+The mad scientist Kelvin has mastered predicting the weather in his mountain-side meteorology lab.
+Recently, Kelvin began publishing his weather forecasts on his website, however, there’s a problem: All of his forecasts describe the temperature in Fahrenheit degrees.
+Let’s convert a temperature from Fahrenheit (F) to Celsius (C).
+
+The formula is the following:
+C=(F−32)/1.8
+*/
+int main() {
+	double tempf = 43;
+	double tempc = (tempf - 32) / 1.8;
+	// print: The temp is [tempc] degrees Celsius.
+	std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+}
